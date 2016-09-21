a6abab25a429c4260b6f14a7c9cfc6bb15b9ac6c
Abilarsh <vabilarsh@hotmail.ca>
2016-09-20 21:28:11 -0400

    spinners and strings

--- app/src/main/java/com/example/vabil/resistorcalculator/MainActivity.java
+++ app/src/main/java/com/example/vabil/resistorcalculator/MainActivity.java
@@ -2,9 +2,8 @@ package com.example.vabil.resistorcalculator;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -12,6 +11,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
@@ -20,28 +20,61 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity
 
         implements NavigationView.OnNavigationItemSelectedListener {
-Spinner spinner;
-    ArrayAdapter<CharSequence> adapter;
+    Spinner spinner;
     Spinner spinner2;
     Spinner spinner3;
+    Spinner spinner4;
+    Spinner spinner5;
+    ArrayAdapter<CharSequence> adapter;
     ArrayAdapter<CharSequence> adapter2;
+    ArrayAdapter<CharSequence> adapter3;
+    int band1;
+    int band2;
+    int band3;
+    int multiplier;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        adapter = ArrayAdapter.createFromResource(this,R.array.resistorColours,android.R.layout.simple_spinner_item);
+        adapter = ArrayAdapter.createFromResource(this, R.array.resistorColours, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-
-        //WHY THIS NO WORKS
-
-     //   adapter2=ArrayAdapter.createFromResource(this.R.array.multiplier,android.R.layout.simple_spinner_item);
-        spinner = (Spinner)findViewById(R.id.band1);
+        adapter2 = ArrayAdapter.createFromResource(this, R.array.multiplier, android.R.layout.simple_spinner_item);
+        adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        adapter3 = ArrayAdapter.createFromResource(this, R.array.tolerance, android.R.layout.simple_spinner_item);
+        adapter3.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner = (Spinner) findViewById(R.id.band1);
         spinner.setAdapter(adapter);
         spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-              Toast.makeText(getBaseContext(),parent.getItemAtPosition(position)+" selected",Toast.LENGTH_LONG).show();
+                //Toast.makeText(getBaseContext(),parent.getItemAtPosition(position)+" selected",Toast.LENGTH_LONG).show();
+                if (parent.getItemAtPosition(position) == "Black") {
+                    band1 = 0;
+                } else if (parent.getItemAtPosition(position) == "Brown") {
+                    band1 = 1;
+                } else if (parent.getItemAtPosition(position) == "Red") {
+                    band1 = 2;
+                } else if (parent.getItemAtPosition(position) == "Orange") {
+                    band1 = 3;
+                } else if (parent.getItemAtPosition(position) == "Yellow") {
+                    band1 = 4;
+                } else if (parent.getItemAtPosition(position) == "Green") {
+                    band1 = 5;
+                } else if (parent.getItemAtPosition(position) == "blue") {
+                    band1 = 6;
+                } else if (parent.getItemAtPosition(position) == "Violet") {
+                    band1 = 7;
+                } else if (parent.getItemAtPosition(position) == "Grey") {
+                    band1 = 8;
+                } else if (parent.getItemAtPosition(position) == "White") {
+                    band1 = 9;
+                }
+                 Toast.makeText(getBaseContext(),parent.getItemAtPosition(position)+" selected",Toast.LENGTH_LONG).show();
+//                Toast.makeText(getApplicationContext(), band1, Toast.LENGTH_SHORT).show();
+
+              //  Toast.makeText(getBaseContext(), parent.getItemAtPosition(position) + band1, Toast.LENGTH_SHORT).show();
+
             }
 
             @Override
@@ -49,7 +82,7 @@ Spinner spinner;
 
             }
         });
-        spinner2= (Spinner)findViewById(R.id.band2);
+        spinner2 = (Spinner) findViewById(R.id.band2);
         spinner2.setAdapter(adapter);
         spinner2.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
@@ -62,9 +95,35 @@ Spinner spinner;
 
             }
         });
-        spinner3=(Spinner)findViewById(R.id.band3);
+        spinner3 = (Spinner) findViewById(R.id.band3);
         spinner3.setAdapter(adapter);
         spinner3.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+        spinner4 = (Spinner) findViewById(R.id.multiplier);
+        spinner4.setAdapter(adapter2);
+        spinner4.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+        spinner5 = (Spinner) findViewById(R.id.tolerance);
+        spinner5.setAdapter(adapter3);
+        spinner5.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
 
@@ -153,4 +212,5 @@ Spinner spinner;
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
+
 }
